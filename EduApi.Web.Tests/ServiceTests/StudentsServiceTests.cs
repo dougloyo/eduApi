5ec9f70fefcb8d4739c5baf1a83e69225d212dff
Douglas Loyo <douglasloyo@gmail.com>
2017-05-05 17:05:15 -0500

    Filling out Unit tests and implementation.

--- EduApi.Web.Tests/ServiceTests/StudentsServiceTests.cs
+++ EduApi.Web.Tests/ServiceTests/StudentsServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using EduApi.Web.Models;
 using EduApi.Web.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -23,13 +24,63 @@ namespace EduApi.Web.Tests.ServiceTests
         public class When_getting_a_student_by_id
         {
             [TestMethod]
-            [ExpectedException(typeof(NotImplementedException))]
-            public void Should_throw_exception_if_student_id_not_found()
+            public void Should_return_a_the_student_with_the_requested_id()
             {
                 IStudentsService service = new StudentsService();
-                var suppliedStudentId = -1;
+                var suppliedStudentId = 1;
                 var actualModel = service.Get(suppliedStudentId);
-                // No assert is needed because we are expectind an exception
+                Assert.IsTrue(actualModel.Id == suppliedStudentId);
+            }
+        }
+
+        [TestClass]
+        public class When_adding_a_student
+        {
+            [TestMethod]
+            public void Should_return_a_the_student_with_an_id()
+            {
+                IStudentsService service = new StudentsService();
+                var suppliedStudent = new Student { FirstName = "John", LastName = "Doe" };
+                var actualModel = service.Add(suppliedStudent);
+                Assert.IsTrue(actualModel.Id != 0);
+            }
+        }
+
+        [TestClass]
+        public class When_editing_a_student
+        {
+            [TestMethod]
+            public void Should_update_student_info()
+            {
+                IStudentsService service = new StudentsService();
+                
+                // Get a student and update his name.
+                var actualModel = service.Get(1);
+                actualModel.FirstName = "Johnathan";
+
+                // Call the update method
+                service.Update(actualModel);
+
+                // Get the model again to ensure it has been updated.
+                var updatedModel = service.Get(1);
+
+                Assert.IsTrue(actualModel.FirstName == updatedModel.FirstName);
+            }
+        }
+
+        [TestClass]
+        public class When_deleting_a_student
+        {
+            [TestMethod]
+            public void Should_delete_student()
+            {
+                IStudentsService service = new StudentsService();
+
+                service.Delete(1);
+
+                // Get a student and update his name.
+                var actualModel = service.Get(1);
+                Assert.IsNull(actualModel);
             }
         }
     }
--- EduApi.Web/Services/StudentsService.cs
+++ EduApi.Web/Services/StudentsService.cs
@@ -16,33 +16,42 @@ namespace EduApi.Web.Services
     }
     public class StudentsService : IStudentsService
     {
-        public IEnumerable<Student> Get()
-        {
-            return new[]
+        List<Student> _students = new List<Student>
             {
                 new Student { Id = 1, StudentId = "U101", FirstName = "John", LastName = "Doe", DateOfBirth = DateTime.Parse("1/1/2009"), Gender = 1, Grade = 2, Email = "john@mail.com" },
                 new Student { Id = 2, StudentId = "U102", FirstName = "Jane", LastName = "Doe", DateOfBirth = DateTime.Parse("1/1/2009"), Gender = 2, Grade = 2, Email = "jane@mail.com" },
             };
+
+        public IEnumerable<Student> Get()
+        {
+            return _students;
         }
 
         public Student Get(int id)
         {
-            throw new NotImplementedException();
+            var model = _students.SingleOrDefault(x => x.Id == id);
+            return model;
         }
 
         public Student Add(Student model)
         {
-            throw new NotImplementedException();
+            var maxId = _students.Max(x => x.Id);
+            model.Id = maxId+1;
+            _students.Add(model);
+            return model;
         }
 
         public void Update(Student model)
         {
-            throw new NotImplementedException();
+            var modelToUpdate = _students.SingleOrDefault(x => x.Id == model.Id);
+
+            modelToUpdate.FirstName = model.FirstName;
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var modelToDelete = _students.SingleOrDefault(x => x.Id == id);
+            _students.Remove(modelToDelete);
         }
     }
 }
