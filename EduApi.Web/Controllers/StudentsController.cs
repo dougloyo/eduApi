7667dc4172de5c080e4ff47871356e0bada83d6a
Douglas Loyo <douglasloyo@gmail.com>
2017-05-05 15:28:06 -0500

    Updating Student controller to be async.

--- EduApi.Web/Controllers/StudentsController.cs
+++ EduApi.Web/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.IO;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace EduApi.Web.Controllers
@@ -6,30 +7,43 @@ namespace EduApi.Web.Controllers
     public class StudentsController : ApiController
     {
         // GET: api/Students
-        public IEnumerable<string> Get()
+        public async Task<IHttpActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            // await a new task.
+            var model = await Task.Run(()=> new [] { "value1", "value2" });
+
+            return Ok(model);
         }
 
         // GET: api/Students/5
-        public string Get(int id)
+        public async Task<IHttpActionResult> Get(int id)
         {
-            return "value";
+            var model = await Task.Run(() => "Value");
+            return Ok(model);
         }
 
         // POST: api/Students
-        public void Post([FromBody]string value)
+        public async Task<IHttpActionResult> Post([FromBody]string value)
         {
+            var model = value;
+            
+            var createdLocation = Path.Combine(Request.RequestUri.ToString(), model);
+
+            return Created(createdLocation, model);
         }
 
         // PUT: api/Students/5
-        public void Put(int id, [FromBody]string value)
+        public async Task<IHttpActionResult> Put(int id, [FromBody]string requestModel)
         {
+            // For a put action we return Ok with no content.
+            return Ok();
         }
 
         // DELETE: api/Students/5
-        public void Delete(int id)
+        public async Task<IHttpActionResult> Delete(int id)
         {
+            // Return Ok no content.
+            return Ok();
         }
     }
 }
