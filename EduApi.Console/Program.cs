9c03d841508f34efe076dc217a0d4f2d258f9f78
Austin Waters <austinwaters16@yahoo.com>
2017-09-25 12:17:47 -0500

    Added option to host as a console app

---
+++ EduApi.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using EduApi.Web;
+using Microsoft.Owin.Hosting;
+
+namespace EduApi.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = ConfigurationManager.AppSettings["BaseAddress"];
+
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Process.Start(baseAddress);
+                System.Console.WriteLine($"Started on {baseAddress}...");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
